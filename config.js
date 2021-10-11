47e49190f9e7eb993c4ba21b124154b573a5067f
huhao <18255606178@163.com>
2021-10-11 17:50:46 +0800

    init

---
+++ config.js
@@ -0,0 +1,13 @@
+// 配置信息
+module.exports = {
+  fromDisplayText: '自动发送', // 收件箱展示的来件人名字
+  fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
+  user: '1025554991@qq.com', // 发送者邮箱
+  pass: 'zwafhhqnlhbybcia', // 发送者邮箱MTP协议密码
+  to: '18255606178@163.com', // 发送到谁，填邮箱
+  weatherKey: '3fac017025db48c481581da7f5732897', // 和风天气key
+  location: '101220101',  // 和风天气-成都武侯区的id
+  type: '1,3,9', // 和风天气-生活指数type
+  tianXingKey: 'edb266add1e4eefacb3dbf6911b10549', // 天行数据的key
+  startDay: '2020-09-29', // 在一起的日期
+}
