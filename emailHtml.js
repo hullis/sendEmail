c34f2f3abcfefa34ea290c1971f0f5f09ffba125
huhao <45870928+hullis@users.noreply.github.com>
2021-10-13 10:52:23 +0800

    Update emailHtml.js

--- emailHtml.js
+++ emailHtml.js
@@ -1,7 +1,15 @@
 function fn(weatherData, lifeData, word, imgurl, lovingDays) {
   const { daily: weatherDataDaily } = weatherData;
   const { daily } = lifeData;
-
+  let html = '';
+  for(var i=0;i<daily.length;i++) {
+    html += `
+    <li style="margin-bottom: 10px">
+      ${daily[i].name}(${daily[i].category}):
+      ${daily[i].text}
+    </li>`
+  }
+  
   return `<!DOCTYPE html>
   <html lang="en">
     <head>
@@ -11,10 +19,6 @@ function fn(weatherData, lifeData, word, imgurl, lovingDays) {
     </head>
     <body>
       <div>
-        <!-- 天数 -->
-        <div>
-          <p>今天是在一起的第${lovingDays}天！</p>
-        </div>
         <!-- 图片 -->
         <div>
           <img
@@ -36,18 +40,7 @@ function fn(weatherData, lifeData, word, imgurl, lovingDays) {
             <span>${weatherDataDaily[0].tempMin}°C - ${weatherDataDaily[0].tempMax}°C</span>
           </p>
           <ul>
-            <li style="margin-bottom: 10px">
-              ${daily[1].name}(${daily[1].category}):
-              ${daily[1].text}
-            </li>
-            <li style="margin-bottom: 10px">
-              ${daily[2].name}(${daily[2].category}):
-              ${daily[2].text}
-            </li>
-            <li style="margin-bottom: 10px">
-              ${daily[0].name}(${daily[0].category}):
-              ${daily[0].text}
-            </li>
+          ` + html + `
           </ul>
         </div>
       </div>
