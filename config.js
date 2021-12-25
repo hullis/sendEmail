ab9a2787e35b2d195ac9123b51b062527bd987e0
huhao <45870928+warmthlinn@users.noreply.github.com>
2021-12-25 17:20:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 module.exports = {
   fromDisplayText: '', // 收件箱展示的来件人名字
   fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
-  user: '1025554991@qq.com', // 发送者邮箱
-  pass: 'zwafhhqnlhbybcia', // 发送者邮箱MTP协议密码
-  to: '420378410@qq.com', // 发送到谁，填邮箱
-  weatherKey: '3fac017025db48c481581da7f5732897', // 和风天气key
+  user: process.env.SENDEMAIL, // 发送者邮箱
+  pass: process.env.EMAILPASS, // 发送者邮箱MTP协议密码
+  to: process.env.TOEMAIL, // 发送到谁，填邮箱
+  weatherKey: process.env.WEATHERKEY, // 和风天气key
   location: '101220101',  // 和风天气-成都武侯区的id
   type: '0', // 和风天气-生活指数type
-  tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
+  tianXingKey: process.env.TIANXINGKEY, // 天行数据的key
   startDay: '2020-09-29', // 日期
 }
